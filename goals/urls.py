f91e69c33e7e5329699cb776c4c83d34fd4e1c1c
Katie Adaline Sroub <ksroub@gmail.com>
2018-12-19 23:39:17 -0500

    update urls and views

--- goals/urls.py
+++ goals/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
+    # ex: /goals/
     path('', views.index, name='index'),
+    # ex: /goals/1/
+    path('<int:goallist_id>', views.detail, name='detail'),
+    # ex /goals/1/add/
+    path('<int:goallist_id>/add/', views.add, name='vote'),
+
 ]
--- goals/views.py
+++ goals/views.py
@@ -3,5 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def index(request):
     return HttpResponse("Hello, world. You're at the goals index.")
+
+
+def detail(request, goallist_id):
+    return HttpResponse("You're looking at %s." % goallist_id)
+
+
+def add(request, goallist_id):
+    return HttpResponse("You're adding to %s." % goallist_id)
